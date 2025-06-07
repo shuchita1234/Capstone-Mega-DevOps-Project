a0ea9370b3aa46301dfc3eb2e83803504945150b
Shuchita Chiwande <145900918+shuchita1234@users.noreply.github.com>
2025-06-07 11:49:45 +0530

    Update BankappApplicationTests.java

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -15,12 +15,7 @@
 package com.example.bankapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootTest
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 class BankappApplicationTests {
 
     @Test
